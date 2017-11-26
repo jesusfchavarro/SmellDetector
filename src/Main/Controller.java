fb0936f78d649c09e8bc26825d2de12bda614e37
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2017-11-25 23:46:46 -0500

    update git ignore

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -1,4 +1,17 @@
 package Main;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
 public class Controller {
+    public WebView webView;
+
+
+    public void load(ActionEvent actionEvent) {
+        WebEngine webEngine = webView.getEngine();
+        webEngine.load("https://refactoring.guru/refactoring/smells");
+
+    }
 }
